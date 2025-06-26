eda2fd4c19b3841705ca270534fca7c9a3416391
Karine Santos <janeissakarine@gmail.com>
2025-06-26 14:15:52 -0300

    Prática 03 – Parte 1 – Criando uma lista simples

--- app/src/main/java/com/ifpe/weatherapp/ListPage.kt
+++ app/src/main/java/com/ifpe/weatherapp/ListPage.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
 import android.widget.Toast
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -27,7 +26,6 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
