6702ca6309fce6a5a78aba620e076a70a3b7c220
Karine Santos <janeissakarine@gmail.com>
2025-08-02 14:02:20 -0300

    Prática 09 – Parte 2 - Carregando imagens das condições climáticas no mapa (BUG AO CLICAR NO MAPA - NÃO ADICIONA CIDADE E APP TRAVA)

--- app/src/main/java/com/ifpe/weatherapp/MapPage.kt
+++ app/src/main/java/com/ifpe/weatherapp/MapPage.kt
@@ -11,10 +11,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.scale
 import com.ifpe.weatherapp.model.MainViewModel
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -27,27 +31,39 @@ import com.google.maps.android.compose.rememberCameraPositionState
 @Composable
 fun ComposableMapPage(modifier: Modifier = Modifier, viewModel: MainViewModel) {
     val cityList = viewModel.cities
-    val camPosState = rememberCameraPositionState ()
+    val camPosState = rememberCameraPositionState()
+    val context = LocalContext.current
 
     GoogleMap (
         modifier = modifier.fillMaxSize(),
         onMapClick = {
-//            viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it)
             viewModel.add(location = it)
         },
         cameraPositionState = camPosState
     )  {
-            viewModel.cities.forEach {
-                if (it.location != null) {
-                    LaunchedEffect(it.name) {
-                        if (it.weather == null) {
-                            viewModel.loadWeather(it.name)
-                        }
+        viewModel.cities.forEach {
+            if (it.location != null) {
+                LaunchedEffect(it.name) {
+                    if (it.weather == null) {
+                        viewModel.loadWeather(it.name)
                     }
-                    Marker( state = MarkerState(position = it.location!!),
-                        title = it.name,
-                        snippet = it.weather?.desc?:"Carregando...")
                 }
+                LaunchedEffect(it.weather) {
+                    if (it.weather != null && it.weather!!.bitmap == null) {
+                        viewModel.loadBitmap(it.name)
+                    }
+                }
+                val image = it.weather?.bitmap ?:
+                getDrawable(context, R.drawable.loading)!!
+                    .toBitmap()
+                val marker = BitmapDescriptorFactory
+                    .fromBitmap(image.scale(120,120))
+                Marker( state = MarkerState(position = it.location),
+                    title = it.name,
+                    icon = marker,
+                    snippet = it.weather?.desc?:"Carregando..."
+                )
             }
+        }
     }
 }
