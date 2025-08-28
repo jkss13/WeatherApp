f9fbd7ad2a98c629de713d7ad766cfb5a32600d0
Karine Santos <janeissakarine@gmail.com>
2025-08-28 15:16:47 -0300

    Prática 12 – Parte 3 - Atualizando o Repositório e ViewModel

--- app/src/main/java/com/ifpe/weatherapp/repo/Repository.kt
+++ app/src/main/java/com/ifpe/weatherapp/repo/Repository.kt
@@ -1,77 +1,57 @@
 package com.ifpe.weatherapp.repo
 
-import com.ifpe.weatherapp.db.fb.FBCity
 import com.ifpe.weatherapp.db.fb.FBDatabase
-import com.ifpe.weatherapp.db.fb.FBUser
-import com.ifpe.weatherapp.db.fb.toFBCity
 import com.ifpe.weatherapp.db.local.LocalDatabase
+import com.ifpe.weatherapp.db.fb.toFBCity
 import com.ifpe.weatherapp.db.local.toCity
 import com.ifpe.weatherapp.db.local.toLocalCity
 import com.ifpe.weatherapp.model.City
 import com.ifpe.weatherapp.model.User
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
-class Repository ( private val fbDB: FBDatabase,
-                   private val localDB : LocalDatabase) : FBDatabase.Listener {
-
-    interface Listener {
-        fun onUserLoaded(user: User)
-        fun onUserSignOut()
-        fun onCityAdded(city: City)
-        fun onCityUpdated(city: City)
-        fun onCityRemoved(city: City)
-    }
-
-    private var listener : Listener? = null
-
-    fun setListener(listener: Listener? = null) {
-        this.listener = listener
-    }
-
-    private var ioScope : CoroutineScope = CoroutineScope(Dispatchers.IO)
+class Repository(
+    private val fbDB: FBDatabase,
+    private val localDB: LocalDatabase
+) {
+    private var ioScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
     private var cityMap = emptyMap<String, City>()
 
-    init {
-        fbDB.setListener(this)
+    val cities = localDB.getCities().map { list ->
+        list.map { city -> city.toCity() }
+    }
+    val user = fbDB.user.map { it.toUser() }
 
+    init {
         ioScope.launch {
-            localDB.getCities().collect { localCityList ->
-                val cityList = localCityList.map { it.toCity() }
+            fbDB.cities.collect { fbCityList ->
+                val cityList = fbCityList.map { it.toCity() }
                 val nameList = cityList.map { it.name }
-                val deletedCities = cityMap.filter { it.key !in  nameList}.values
+
+                val deletedCities = cityMap.filter { it.key !in nameList }
                 val updatedCities = cityList.filter { it.name in cityMap.keys }
                 val newCities = cityList.filter { it.name !in cityMap.keys }
 
-                newCities.forEach { listener?.onCityAdded(it) }
-                updatedCities.forEach { listener?.onCityUpdated(it) }
-                deletedCities.forEach { listener?.onCityRemoved(it) }
+                newCities.forEach { localDB.insert(it.toLocalCity()) }
+                updatedCities.forEach { localDB.update(it.toLocalCity()) }
+                deletedCities.forEach { localDB.delete(it.value.toLocalCity()) }
 
                 cityMap = cityList.associateBy { it.name }
             }
         }
     }
 
-    fun add(city: City) = fbDB.add(city.toFBCity())
-
-    fun remove(city: City) = fbDB.remove(city.toFBCity())
-
-    fun update(city: City) = fbDB.update(city.toFBCity())
-
-    override fun onUserLoaded(user: FBUser)= listener?.onUserLoaded(user.toUser())?:Unit
-
-    override fun onUserSignOut() = listener?.onUserSignOut()?:Unit
-
-    override fun onCityAdded(city: FBCity) {
-        localDB.insert(city.toCity().toLocalCity())
+    fun add(city: City) = ioScope.launch {
+        fbDB.add(city.toFBCity())
     }
 
-    override fun onCityUpdated(city: FBCity) {
-        localDB.update(city.toCity().toLocalCity())
+    fun remove(city: City) = ioScope.launch {
+        fbDB.remove(city.toFBCity())
     }
 
-    override fun onCityRemoved(city: FBCity) {
-        localDB.delete(city.toCity().toLocalCity())
+    fun update(city: City) = ioScope.launch {
+        fbDB.update(city.toFBCity())
     }
 }
